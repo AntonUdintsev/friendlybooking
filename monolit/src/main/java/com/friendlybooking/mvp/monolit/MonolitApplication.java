843790277be1348763ebe5955ae7b365a69de33a
AntonUdintsev <antonudincev@gmail.com>
2024-10-24 01:16:16 +0300

    first microservice

---
+++ monolit/src/main/java/com/friendlybooking/mvp/monolit/MonolitApplication.java
@@ -0,0 +1,13 @@
+package com.friendlybooking.mvp.monolit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolitApplication.class, args);
+    }
+
+}
