129c3162f4ebae96c4bc2f09de07358c91e238e5
AntonUdintsev <antonudincev@gmail.com>
2024-10-24 22:22:09 +0300

    first microservice started

--- auth-service/src/main/java/com/friendlybooking/mvp/authservice/SecurityConfig.java
+++ auth-service/src/main/java/com/friendlybooking/mvp/authservice/SecurityConfig.java
@@ -3,30 +3,34 @@ package com.friendlybooking.mvp.authservice;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .anyRequest().authenticated()
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/public/**", "/login").permitAll() // Public routes
+                                .anyRequest().authenticated() // All other routes require authentication
                 )
-                .oauth2Login(oauth2Login ->
-                        oauth2Login
-                                .defaultSuccessUrl("/dashboard", true)
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
                 )
-                .logout(logout ->
-                        logout
-                                .logoutSuccessUrl("/login?logout=true")
-                                .permitAll()
-                );
+                .oauth2Login(); // Enable OAuth2 login with Keycloak
+
         return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        // Configure custom JWT conversion if needed
+        return jwtAuthenticationConverter;
     }
 }
